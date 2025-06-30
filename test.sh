47ad29ce9082e1831f37fda560525b7dbb62fed6
0xgeeb <0xgeeb@gmail.com>
2025-06-30 16:47:29 -0400

    modified test

--- test.sh
+++ test.sh
@@ -22,6 +22,6 @@
 
 # curl -X GET "http://localhost:3001/infrared/5885759" -w "\n"
 
-curl -X GET "http://localhost:3001/0x66090e34c9192Ee9927f44f978246be3e5365D36/6907747" -w "\n"
+# curl -X GET "http://localhost:3001/0x66090e34c9192Ee9927f44f978246be3e5365D36/6907747" -w "\n"
 
-# curl -X GET "https://api.goldilocks.io/0x66090e34c9192Ee9927f44f978246be3e5365D36/6907747" -w "\n"
+curl -X GET "https://api.goldilocks.io/0x66090e34c9192Ee9927f44f978246be3e5365D36/6907747" -w "\n"
